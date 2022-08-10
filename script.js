0baa809c27380e181cb51c45427435d3f882de20
Varesva <varesva.dev@gmail.com>
2022-08-10 09:41:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// pour avoir l'année actualisée du copyright 
+let dateCopyright = new Date();
+let yearCopyright = dateCopyright.getFullYear();
+document.getElementById("year").innerHTML = yearCopyright;
+
