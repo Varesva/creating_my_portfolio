b76438809919f54dcecd7a2c166f4004ef6e640c
Varesva <varesva.dev@gmail.com>
2022-08-11 16:07:15 +0200

    arrows added and sized, matched fonts

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let dateCopyright = new Date();
 let yearCopyright = dateCopyright.getFullYear();
 document.getElementById("year").innerHTML = yearCopyright;
 
+// SHOW AND HIDE
+const mediaQueryArrows = window.matchMedia("(min-width: 1024px)");
+
+const divArrowDown = document.getElementsByClassName("full-arrow-down");
+// console.log(divArrowDown);   => 3 élements
+
+// Boucler sur chaque élement
+for (const arrow of divArrowDown) {
+    if (mediaQueryArrows.matches) {
+        arrow.style.display = "none";
+    } else {
+        arrow.style.display = "block";
+    };  
+};
